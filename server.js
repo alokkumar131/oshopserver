40e675105083502a134e7819a7646e92281c49ea
alokkumar131 <dmalok8@gmail.com>
2018-10-01 00:32:29 -0700

    msg

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const path=require('path');
 //Set Port
 const port=process.env.PORT | 3000;
 // API file for interacting with MongoDB
-const api = require('./server/routes/api');
+// const api = require('/server/routes/api');
 var app=express();
+
 
 // Parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 // API location
-app.use('/', api);
+// app.use('/', api);
+app.get('/',(req,res)=>{
+    res.send('api works');
+});
 
 //Server start
 app.listen(port,(req,res)=>{
