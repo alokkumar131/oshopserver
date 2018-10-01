cd897b9c59e1d9caea6a5fe2273340f487cec8f7
alokkumar131 <dmalok8@gmail.com>
2018-09-30 23:41:32 -0700

    'servercreated'

---
+++ server.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const path=require('path');
+//Set Port
+const port=process.env.PORT | 3000;
+// API file for interacting with MongoDB
+const api = require('./server/routes/api');
+var app=express();
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// API location
+app.use('/api', api);
+
+//Server start
+app.listen(port,(req,res)=>{
+    console.log('Server started on port number :'+port)
+})
